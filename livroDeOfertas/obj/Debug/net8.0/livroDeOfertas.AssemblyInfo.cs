a493f0991102a114fb7e6d26dc9e8a6100c35059
samuxka <samukactto@gmail.com>
2024-06-14 23:55:52 -0300

    02quese

--- livroDeOfertas/obj/Debug/net8.0/livroDeOfertas.AssemblyInfo.cs
+++ livroDeOfertas/obj/Debug/net8.0/livroDeOfertas.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("mensagemCriptografada")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("livroDeOfertas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f46787fdb861bf9c5504da0b860970235e1e8f")]
-[assembly: System.Reflection.AssemblyProductAttribute("mensagemCriptografada")]
-[assembly: System.Reflection.AssemblyTitleAttribute("mensagemCriptografada")]
+[assembly: System.Reflection.AssemblyProductAttribute("livroDeOfertas")]
+[assembly: System.Reflection.AssemblyTitleAttribute("livroDeOfertas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
