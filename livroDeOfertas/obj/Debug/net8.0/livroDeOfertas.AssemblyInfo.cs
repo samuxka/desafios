33cf63f8fb105dde24d52e684675b05b299700e4
samuxka <samukactto@gmail.com>
2024-06-17 20:22:38 -0300

    oie

--- livroDeOfertas/obj/Debug/net8.0/livroDeOfertas.AssemblyInfo.cs
+++ livroDeOfertas/obj/Debug/net8.0/livroDeOfertas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("livroDeOfertas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f46787fdb861bf9c5504da0b860970235e1e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a493f0991102a114fb7e6d26dc9e8a6100c35059")]
 [assembly: System.Reflection.AssemblyProductAttribute("livroDeOfertas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("livroDeOfertas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
